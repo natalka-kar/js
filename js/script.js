41f75084afa7ae75a6aa334cd0eaa795480d174a
natalka-kar <nataryades@gmail.com>
2022-05-22 20:29:58 +0300

    add prommpt

---
+++ js/script.js
@@ -0,0 +1,19 @@
+ const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+ const personalMovieDB = {
+     count: numberOfFilms,
+     movies: {},
+     actors: {},
+     genres: [],
+     privat: false
+ };
+
+ const a = prompt('Один из последних просмотренный фильмов', ''),
+       b = prompt('На сколько оцените тего?', ''),
+       c = prompt('Один из последних просмотренный фильмов', ''),
+       d = prompt('На сколько оцените тего?', '');
+
+personalMovieDB.movies[a] = b; 
+personalMovieDB.movies[c] = d; 
+
+console.log(personalMovieDB);
